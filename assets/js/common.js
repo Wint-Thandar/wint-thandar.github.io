d5bd6595cdfd309c9ef572b77dd7987e4e4d5e4d
Wint-Thandar <Wint-Thandar@users.noreply.github.com>
2023-11-30 18:25:04 +0000

    jekyll build from Action d52ca9b0c6ab49f23f42588fcfb8d029f02a87eb

---
+++ assets/js/common.js
@@ -0,0 +1,61 @@
+$(function() {
+  'use strict';
+
+  /* =======================
+  // Reveal Image
+  ======================= */
+  var wh = window.innerHeight;
+
+  $(window).ready(function () {
+    $('body').waitForImages({
+      finished: function () {
+        setTimeout(function () {
+          $('.preloader').addClass('hide');
+
+          setTimeout(function () {
+            reveals();
+          }, 100);
+        }, 500);
+      },
+      waitForAll: true
+    });
+  });
+
+  function reveals() {
+    $(window).on('scroll', function () {
+      $(".article-box, .article-first, .post-image-box, .page-image-box, .post-body img, .page-body img, .recent-header").each(
+        function(i) {
+          var el_top = $(this).offset().top,
+            win_bottom = wh + $(window).scrollTop();
+
+          if (el_top < win_bottom) {
+            $(this)
+              .delay(i * 100)
+              .queue(function() {
+                $(this).addClass("reveal-in");
+              });
+          }
+        }
+      );
+    }).scroll();
+  }
+
+
+  /* =======================
+  // Responsive Videos
+  ======================= */
+  $(".post-content, .page-content").fitVids({
+    customSelector: ['iframe[src*="ted.com"]']
+  });
+
+
+  /* =======================
+  // Scroll Top Button
+  ======================= */
+  $(".top").click(function () {
+    $("html, body")
+      .stop()
+      .animate({ scrollTop: 0 }, "slow", "swing");
+  });
+
+});
